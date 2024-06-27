c9d4b03cd17a7456e73d8e8131bc9c5d21aad6a0
Amama-Fatima <amamafatima58@gmail.com>
2024-06-27 14:02:17 +0500

    fixes for build errors

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,65 +1,63 @@
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import useFetch from '../../../hooks/useFetch';
-import { useSelector } from 'react-redux';
-import Img from '../../../components/lazyloadImage/Img';
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
-import './style.scss'
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import Img from "../../../components/lazyloadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import "./style.scss";
 
 const HeroBanner = () => {
-
-  const [background, setBackground] = useState('');
-  const [query, setQuery] = useState('');
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
-  const {url}= useSelector((state)=>state.home);
+  const { url } = useSelector((state) => state.home);
 
-  const {data, loading} = useFetch('/movie/upcoming');
+  const { data, loading } = useFetch("/movie/upcoming");
 
-
-  useEffect(()=>{
-    const bg = url.backdrop + data?.results[Math.floor(Math.random()*data.results.length)].backdrop_path;
-    setBackground(bg)
-  }, [data])
-
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results[Math.floor(Math.random() * data.results.length)]
+        .backdrop_path;
+    setBackground(bg);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
 
   const searchQueryHandler = (event) => {
-    if(event.key==='Enter' && query.length>0){
-      navigate(`/search/${query}`)
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
     }
-  }
-
-
-
-
+  };
 
   return (
-    <div className='heroBanner'>
-      { !loading && <div className="backdrop-img">
-        <Img src={background} />
-      </div>}
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
 
-      <div className="opacity-layer">
-
-      </div>
+      <div className="opacity-layer"></div>
 
       <ContentWrapper>
-      
-     
         <div className="heroBannerContent">
           <span className="title">Welcome</span>
-          <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
           <div className="searchInput">
-            <input type="text" placeholder="Search for a movie, tv show......" onKeyUp={searchQueryHandler}  onChange={(e)=> setQuery(e.target.value)}/>
+            <input
+              type="text"
+              placeholder="Search for a movie, tv show......"
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
             <button>Search</button>
           </div>
         </div>
-      
-
       </ContentWrapper>
-
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
